f51766abaae54a70bd2e64874337414ace7c1af3
Joseph Lassuy <Josephllassuy@gmail.com>
2025-03-01 02:52:27 -0500

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,17 @@
-// app.config.ts
-import { defineConfig } from '@tanstack/react-start/config'
-import { cloudflare } from 'unenv'
+import { defineConfig } from "@tanstack/react-start/config";
+import { cloudflare } from "unenv";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
-    preset: 'cloudflare-pages',
-    unenv: cloudflare,
+    preset: "cloudflare-pages",
+    unenv: cloudflare
   },
-})
+  vite: {
+    plugins: [
+      tsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+    ],
+  },
+});
