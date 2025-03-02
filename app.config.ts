a0f301e04c23497c82d096b943585f578da50fe1
Joseph Lassuy <Josephllassuy@gmail.com>
2025-03-02 03:21:29 -0500

    Cloudflare work please anythng

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "@tanstack/react-start/config";
+import { cloudflare } from 'unenv'
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
-    preset: "node-server",
+    preset: 'cloudflare-pages',
+    unenv: cloudflare,
   },
   vite: {
     plugins: [
